072f6fca8bed83fdbf8b9c3286ca2213a0d7dc83
Craig Callender <125502786+CraigCallender@users.noreply.github.com>
2024-10-23 10:44:12 +0100

    [Feature] Devcontainer support and configuration (#3)
    * Adding a devcontainer config.
    * engineering improvements.

--- solis_control_battery_charge.py
+++ solis_control_battery_charge.py
@@ -9,31 +9,37 @@ from datetime import datetime, timezone
 
 VERB = "POST"
 LOGIN_URL = '/v2/api/login'
-CONTROL_URL= '/v2/api/control'
-INVERTER_URL= '/v1/api/inverterList'
+CONTROL_URL = '/v2/api/control'
+INVERTER_URL = '/v1/api/inverterList'
 
-session = async_get_clientsession(hass)
+session = async_get_clientsession(hass)  # type: ignore
+
 
 def digest(body: str) -> str:
-    return base64.b64encode(hashlib.md5(body.encode('utf-8')).digest()).decode('utf-8')
+    return base64.b64encode(hashlib.md5(body.encode('utf-8'))
+                            .digest()) \
+                            .decode('utf-8')
+
 
 def passwordEncode(password: str) -> str:
     md5Result = hashlib.md5(password.encode('utf-8')).hexdigest()
     return md5Result
 
-def prepare_header(config: dict[str,str], body: str, canonicalized_resource: str) -> dict[str, str]:
+
+def prepare_header(config: dict[str, str], body: str,
+                   canonicalized_resource: str) -> dict[str, str]:
     content_md5 = digest(body)
     content_type = "application/json"
-    
+
     now = datetime.now(timezone.utc)
     date = now.strftime("%a, %d %b %Y %H:%M:%S GMT")
-    
+
     encrypt_str = (VERB + "\n"
-        + content_md5 + "\n"
-        + content_type + "\n"
-        + date + "\n"
-        + canonicalized_resource
-    )
+                   + content_md5 + "\n"
+                   + content_type + "\n"
+                   + date + "\n"
+                   + canonicalized_resource
+                   )
     hmac_obj = hmac.new(
         config["secret"].encode('utf-8'),
         msg=encrypt_str.encode('utf-8'),
@@ -41,69 +47,88 @@ def prepare_header(config: dict[str,str], body: str, canonicalized_resource: str
     )
     sign = base64.b64encode(hmac_obj.digest())
     authorization = "API " + config["key_id"] + ":" + sign.decode('utf-8')
-    
+
     header = {
-        "Content-MD5":content_md5,
-        "Content-Type":content_type,
-        "Date":date,
-        "Authorization":authorization
+        "Content-MD5": content_md5,
+        "Content-Type": content_type,
+        "Date": date,
+        "Authorization": authorization
     }
     return header
 
+
 async def login(config):
-    body = '{"userInfo":"'+config['username']+'","password":"'+ passwordEncode(config['password'])+'"}'
+    body = '{"userInfo":"'+config['username'] + \
+        '","password":"' + passwordEncode(config['password'])+'"}'
     header = prepare_header(config, body, LOGIN_URL)
-    response = await session.post("https://www.soliscloud.com:13333"+LOGIN_URL, data = body, headers = header)
+    response = await session.post("https://www.soliscloud.com:13333"
+                                  + LOGIN_URL,
+                                  data=body,
+                                  headers=header)
     status = response.status
     result = ""
-    r = json.loads(re.sub(r'("(?:\\?.)*?")|,\s*([]}])', r'\1\2', response.text()))
+    r = json.loads(re.sub(r'("(?:\\?.)*?")|,\s*([]}])',
+                   r'\1\2', response.text()))
     if status == HTTPStatus.OK:
         result = r
     else:
         log.warning(status)
         result = response.text()
-    
+
     return result["csrfToken"]
 
+
 async def getInverterList(config):
-    body = '{"stationId":"'+config['plantId']+'"}'
+    body = '{"stationId":"' + config['plantId'] + '"}'
     header = prepare_header(config, body, INVERTER_URL)
-    response = await session.post("https://www.soliscloud.com:13333"+INVERTER_URL, data = body, headers = header)
+
+    response = await session.post("https://www.soliscloud.com:13333"
+                                  + INVERTER_URL,
+                                  data=body,
+                                  headers=header)
+
     inverterList = response.json()
     inverterId = ""
+
     for record in inverterList['data']['page']['records']:
-      inverterId = record.get('id')
+        inverterId = record.get('id')
+
     return inverterId
 
 
 def control_battery_charge_body(inverterId, chargeSettings) -> str:
-    body = '{"inverterId":"'+inverterId+'", "cid":"103","value":"'
+    body = '{"inverterId":"' + inverterId + '", "cid":"103","value":"'
     for index, time in enumerate(chargeSettings):
         body = body \
-          +str(time['chargeCurrent']) \
-          +"," \
-          +str(time['dischargeCurrent']) \
-          +"," \
-          +str(time['chargeStartTime']) \
-          +"," \
-          +str(time['chargeEndTime']) \
-          +"," \
-          +str(time['dischargeStartTime']) \
-          +"," \
-          +str(time['dischargeEndTime'])
-        if (index !=2):
+            + str(time['chargeCurrent']) \
+            + "," \
+            + str(time['dischargeCurrent']) \
+            + "," \
+            + str(time['chargeStartTime']) \
+            + "," \
+            + str(time['chargeEndTime']) \
+            + "," \
+            + str(time['dischargeStartTime']) \
+            + "," \
+            + str(time['dischargeEndTime'])
+        if (index != 2):
             body = body+","
     return body+'"}'
+
 
 async def set_battery_charge_times(token, inverterId: str, config, times):
     body = control_battery_charge_body(inverterId, times)
     headers = prepare_header(config, body, CONTROL_URL)
-    headers['token']= token
-    response = await session.post("https://www.soliscloud.com:13333"+CONTROL_URL, data = body, headers = headers)
+    headers['token'] = token
+    response = await session.post("https://www.soliscloud.com:13333"
+                                  + CONTROL_URL,
+                                  data=body,
+                                  headers=headers)
     log.warning("solis_control_battery_charge.py response:" + response.text())
-    
-@service   
-async def solis_control_battery_charge(config=None, settings=None): 
-    inverterId= getInverterList(config)
+
+
+@service
+async def solis_control_battery_charge(config=None, settings=None):
+    inverterId = getInverterList(config)
     token = login(config)
     set_battery_charge_times(token, inverterId, config, settings)
